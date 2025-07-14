fc621c85973872e84bfca3550adcd6038ab17ed8
Ethan Long <ethanlong1227@icloud.com>
2025-07-14 14:36:09 -0500

    te

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,130 +1,85 @@
 <template>
   <div id="app">
-    <transition name="slide-out">
-      <div v-if="!showMainContent" class="landing-page" @touchstart="handleTouchStart" @touchend="handleTouchEnd">
-        <!-- Chat animation container -->
-        <div class="chat-container">
-          <transition name="fade-up">
-            <div v-if="showMyTypingIndicator" class="chat-bubble sent typing">
-              <span></span><span></span><span></span>
-            </div>
-          </transition>
-          <transition name="fade-up">
-            <div v-if="showFirstHey" class="chat-bubble sent">Hey</div>
-          </transition>
-          <transition name="fade-up">
-            <div v-if="showLaurenTypingIndicator" class="chat-bubble received typing">
-              <span></span><span></span><span></span>
-            </div>
-          </transition>
-          <transition name="fade-up">
-            <div v-if="showSecondHey" class="chat-bubble received">hey</div>
-          </transition>
-           <transition name="fade-up">
-            <div v-if="showMySecondTypingIndicator" class="chat-bubble sent typing">
-              <span></span><span></span><span></span>
-            </div>
-          </transition>
-          <transition name="fade-up">
-            <div v-if="showThirdMessage" class="chat-bubble sent">What car do you drive I'm curious</div>
-          </transition>
+    <!-- Main content now loads directly -->
+    <div>
+      <!-- Canvas for the fireworks animation -->
+      <canvas id="fireworks-canvas"></canvas>
+      
+      <!-- Weather Display -->
+      <div v-if="weather.temp" class="weather-container">
+        <img :src="weather.iconUrl" alt="Weather icon">
+        <div class="weather-details">
+          <p>{{ weather.temp }}&deg;F in Chicago</p>
+          <p class="weather-description">{{ weather.description }}</p>
         </div>
-
-        <!-- Original landing page content, now animated to appear after the chat -->
-        <transition name="fade">
-          <div v-if="showLandingContent" class="landing-content-wrapper">
-            <h1>Can you believe it?</h1>
-            <button class="arrow-btn" @click="showMainPage">
-              <span>&#8593;</span>
-            </button>
-          </div>
-        </transition>
       </div>
-    </transition>
+      
+      <!-- Notification Button in the top-left corner -->
+      <button @click="subscribeToNotifications" :disabled="isSubscribed" class="notification-btn" :title="isSubscribed ? 'Notifications are on' : 'Enable notifications'">
+        <!-- Bell with slash (Notifications OFF) -->
+        <svg v-if="!isSubscribed" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M6 8a6 6 0 0 1 12 0c0 7 3 9 3 9H3s3-2 3-9"/><path d="M10.3 21a1.94 1.94 0 0 0 3.4 0"/><path d="m2 2 20 20"/></svg>
+        <!-- Regular Bell (Notifications ON) -->
+        <svg v-else xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M6 8a6 6 0 0 1 12 0c0 7 3 9 3 9H3s3-2 3-9"/><path d="M10.3 21a1.94 1.94 0 0 0 3.4 0"/></svg>
+      </button>
 
-    <transition name="fade" mode="out-in">
-      <!-- This wrapper div allows the canvas and content to be siblings -->
-      <div v-if="showMainContent">
-        <!-- Canvas for the fireworks animation is now a sibling to the main content -->
-        <canvas id="fireworks-canvas"></canvas>
-        
-        <!-- Weather Display -->
-        <div v-if="weather.temp" class="weather-container">
-          <img :src="weather.iconUrl" alt="Weather icon">
-          <div class="weather-details">
-            <p>{{ weather.temp }}&deg;F in Chicago</p>
-            <p class="weather-description">{{ weather.description }}</p>
-          </div>
+      <div class="main-content">
+        <img alt="Vue logo" src="https://cdn-icons-gif.flaticon.com/15600/15600693.gif" style="width: 150px; height: auto;">
+        <h1>{{ greetingMessage }}</h1>
+
+        <p @click="toggleText" :class="{'fade': isFading}">
+          {{ loveMessage }}
+        </p>
+
+        <!-- Button container -->
+        <div class="button-container">
+          <button @click="toggleScroll" ref="letterButton">
+            <h3 v-if="!isDropdownOpen">Click to Read My Love Letter</h3>
+            <h3 v-if="isDropdownOpen">Hide My Love Letter</h3>
+          </button>
+          <button @click="openAlbum">
+            <h3>Our Photo Album</h3>
+          </button>
         </div>
-        
-        <!-- Notification Button in the top-left corner -->
-        <button @click="subscribeToNotifications" :disabled="isSubscribed" class="notification-btn" :title="isSubscribed ? 'Notifications are on' : 'Enable notifications'">
-          <!-- Bell with slash (Notifications OFF) -->
-          <svg v-if="!isSubscribed" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M6 8a6 6 0 0 1 12 0c0 7 3 9 3 9H3s3-2 3-9"/><path d="M10.3 21a1.94 1.94 0 0 0 3.4 0"/><path d="m2 2 20 20"/></svg>
-          <!-- Regular Bell (Notifications ON) -->
-          <svg v-else xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M6 8a6 6 0 0 1 12 0c0 7 3 9 3 9H3s3-2 3-9"/><path d="M10.3 21a1.94 1.94 0 0 0 3.4 0"/></svg>
-        </button>
 
-        <div class="main-content">
-          <img alt="Vue logo" src="https://cdn-icons-gif.flaticon.com/15600/15600693.gif" style="width: 150px; height: auto;">
-          <h1>{{ greetingMessage }}</h1>
-
-          <p @click="toggleText" :class="{'fade': isFading}">
-            {{ loveMessage }}
+        <div v-show="isDropdownOpen" class="dropdown-content" ref="letterContent">
+          <p>
+            My Dearest Lauren, <br><br>
+            This past year with you has truly been the best year of my life. Every moment has been filled with more love, joy, and happiness than I could have ever imagined. From the very first day I saw you, I knew my life was about to change in the most beautiful way. <br><br>
+            Thank you for being the most amazing person in my life. I'm beyond grateful for your love, support and kisses, and I can't wait to spend many more wonderful years together. This website will be online forever as a testament to our love, and I'll be making regular updates to celebrate our journey. <br><br>
+            I Love you so much bae, never change. <br><br>
+            Forever yours, <br><br>
+            Ethan Long
           </p>
-
-          <!-- Button container -->
-          <div class="button-container">
-            <button @click="toggleScroll" ref="letterButton">
-              <h3 v-if="!isDropdownOpen">Click to Read My Love Letter</h3>
-              <h3 v-if="isDropdownOpen">Hide My Love Letter</h3>
-            </button>
-            <button @click="openAlbum">
-              <h3>Our Photo Album</h3>
-            </button>
-          </div>
-
-          <div v-show="isDropdownOpen" class="dropdown-content" ref="letterContent">
-            <p>
-              My Dearest Lauren, <br><br>
-              This past year with you has truly been the best year of my life. Every moment has been filled with more love, joy, and happiness than I could have ever imagined. From the very first day I saw you, I knew my life was about to change in the most beautiful way. <br><br>
-              Thank you for being the most amazing person in my life. I'm beyond grateful for your love, support and kisses, and I can't wait to spend many more wonderful years together. This website will be online forever as a testament to our love, and I'll be making regular updates to celebrate our journey. <br><br>
-              I Love you so much bae, never change. <br><br>
-              Forever yours, <br><br>
-              Ethan Long
-            </p>
-          </div>
-
-          <!-- Quote of the Day Section -->
-          <div v-if="quote.text" class="quote-container">
-            <p class="quote-text">"{{ quote.text }}"</p>
-            <p class="quote-author">- {{ quote.author }}</p>
-          </div>
-          <div v-if="isLoadingQuote" class="loading-quote">
-            <p>Generating a special message for you...</p>
-          </div>
-
         </div>
-        
-        <!-- Photo Album Modal -->
-        <transition name="fade">
-          <div v-if="showAlbum" class="album-modal" @click.self="closeAlbum">
-            <div class="album-content">
-              <button class="close-album-btn" @click="closeAlbum">&times;</button>
-              <h2>Our Love</h2>
-              <div v-if="isLoadingPhotos" class="loading-photos">Loading photos...</div>
-              <div v-else class="photo-grid">
-                <div v-for="(photo, index) in photos" :key="index" class="photo-item">
-                  <img :src="photo.url" :alt="photo.caption || 'A photo of us'">
-                  <p v-if="photo.caption">{{ photo.caption }}</p>
-                </div>
+
+        <!-- Quote of the Day Section -->
+        <div v-if="quote.text" class="quote-container">
+          <p class="quote-text">"{{ quote.text }}"</p>
+          <p class="quote-author">- {{ quote.author }}</p>
+        </div>
+        <div v-if="isLoadingQuote" class="loading-quote">
+          <p>Generating a special message for you...</p>
+        </div>
+
+      </div>
+      
+      <!-- Photo Album Modal -->
+      <transition name="fade">
+        <div v-if="showAlbum" class="album-modal" @click.self="closeAlbum">
+          <div class="album-content">
+            <button class="close-album-btn" @click="closeAlbum">&times;</button>
+            <h2>Our Love</h2>
+            <div v-if="isLoadingPhotos" class="loading-photos">Loading photos...</div>
+            <div v-else class="photo-grid">
+              <div v-for="(photo, index) in photos" :key="index" class="photo-item">
+                <img :src="photo.url" :alt="photo.caption || 'A photo of us'">
+                <p v-if="photo.caption">{{ photo.caption }}</p>
               </div>
             </div>
           </div>
-        </transition>
-
-      </div>
-    </transition>
+        </div>
+      </transition>
+    </div>
   </div>
 </template>
 
@@ -132,24 +87,15 @@
 export default {
   data() {
     return {
-      greetingMessage: 'Loading...',
+      greetingMessage: '',
       loveMessage: 'Loading...',
       translatedMessage: '',
       language: '',
       isFading: false,
       isDropdownOpen: false,
-      showMainContent: false,
-      touchStartY: 0,
       animationFrameId: null, 
       fireworksLaunched: 0,
       maxFireworks: 5,
-      showMyTypingIndicator: false,
-      showFirstHey: false,
-      showLaurenTypingIndicator: false,
-      showSecondHey: false,
-      showMySecondTypingIndicator: false,
-      showThirdMessage: false,
-      showLandingContent: false,
       photos: [],
       showAlbum: false,
       isLoadingPhotos: false,
@@ -161,70 +107,57 @@ export default {
     };
   },
   created() {
-    if (localStorage.getItem('hasVisited') === 'true') {
-      this.showMainContent = true;
-    }
-    this.fetchGreeting(); // Fetch the dynamic greeting from the new Lambda
+    this.setGreetingMessage();
     this.fetchLoveMessage();
     this.fetchQuote(); 
     this.fetchWeather();
   },
   mounted() {
-    if (!this.showMainContent) {
-      this.startIntroAnimation();
-    }
+    this.initFireworks();
     this.checkSubscriptionStatus();
   },
-  watch: {
-    showMainContent(isShowing) {
-      if (isShowing) {
-        this.$nextTick(() => this.initFireworks());
-      } else {
-        if (this.animationFrameId) cancelAnimationFrame(this.animationFrameId);
-      }
-    }
-  },
   beforeUnmount() {
-    if (this.animationFrameId) cancelAnimationFrame(this.animationFrameId);
+    if (this.animationFrameId) {
+        cancelAnimationFrame(this.animationFrameId);
+    }
     window.removeEventListener('resize', this.resizeCanvas);
   },
   methods: {
     triggerHapticFeedback() {
       if ('vibrate' in navigator) navigator.vibrate(50);
     },
-    async fetchGreeting() {
-      try {
-        const response = await fetch('https://0iumgzsw35.execute-api.us-east-2.amazonaws.com/lovestage/get-daily-greeting');
-        if (!response.ok) throw new Error('Failed to fetch greeting');
-        const data = await response.json();
-        this.greetingMessage = data.greeting;
-      } catch (error) {
-        console.error("Error fetching greeting:", error);
-        this.greetingMessage = "Hello, my love!"; // A friendly fallback message
+    setGreetingMessage() {
+      const today = new Date();
+      const month = today.getUTCMonth() + 1;
+      const day = today.getUTCDate();
+      const year = today.getUTCFullYear();
+
+      const getThanksgiving = (year) => {
+        const novemberFirst = new Date(Date.UTC(year, 10, 1));
+        const dayOfWeek = novemberFirst.getUTCDay();
+        const firstThursday = 1 + (4 - dayOfWeek + 7) % 7;
+        return firstThursday + 21;
+      };
+
+      if (month === 7 && day === 14) {
+        this.greetingMessage = "Happy Anniversary, my love!";
+      } else if (month === 7 && day === 6) {
+        this.greetingMessage = "Happy Birthday, my love!";
+      } else if (month === 1 && day === 1) {
+        this.greetingMessage = "Happy New Year, Lauren!";
+      } else if (month === 7 && day === 4) {
+        this.greetingMessage = "Happy Fourth of July!";
+      } else if (month === 11 && day === getThanksgiving(year)) {
+        this.greetingMessage = "Happy Thanksgiving, my love!";
+      } else if (month === 12 && day === 25) {
+        this.greetingMessage = "Merry Christmas, Lauren!";
+      } else {
+        const dayOfWeek = today.toLocaleDateString('en-US', { weekday: 'long', timeZone: 'UTC' });
+        const nicknames = ["bae", "babe", "baby", "love", "Lauren", "cutie", "wife bae", "cutie bae"];
+        const dailyIndex = day % nicknames.length;
+        const dailyNickname = nicknames[dailyIndex];
+        this.greetingMessage = `Have a great ${dayOfWeek}, ${dailyNickname}!`;
       }
-    },
-    startIntroAnimation() {
-      setTimeout(() => { this.showMyTypingIndicator = true; }, 1000);
-      setTimeout(() => { 
-        this.showMyTypingIndicator = false;
-        this.showFirstHey = true; 
-      }, 2500);
-      setTimeout(() => { this.showLaurenTypingIndicator = true; }, 3700);
-      setTimeout(() => {
-        this.showLaurenTypingIndicator = false;
-        this.showSecondHey = true;
-      }, 5500);
-      setTimeout(() => { this.showMySecondTypingIndicator = true; }, 6700);
-      setTimeout(() => { 
-        this.showMySecondTypingIndicator = false;
-        this.showThirdMessage = true; 
-      }, 8200);
-      setTimeout(() => { this.showLandingContent = true; }, 9500);
-    },
-    showMainPage() {
-      this.triggerHapticFeedback();
-      localStorage.setItem('hasVisited', 'true');
-      this.showMainContent = true;
     },
     toggleDropdown() {
       this.isDropdownOpen = !this.isDropdownOpen;
@@ -330,16 +263,6 @@ export default {
           this.$refs.letterButton.scrollIntoView({ behavior: 'smooth', block: 'center' });
         }
       });
-    },
-    handleTouchStart(event) {
-      this.touchStartY = event.touches[0].clientY;
-    },
-    handleTouchEnd(event) {
-      const touchEndY = event.changedTouches[0].clientY;
-      if (touchEndY < this.touchStartY - 50) { 
-        this.triggerHapticFeedback();
-        this.showMainPage();
-      }
     },
     async checkSubscriptionStatus() {
       if ('serviceWorker' in navigator) {
@@ -550,140 +473,7 @@ export default {
     background-color: #f0f0f0;
 }
 
-
-/* --- CHAT ANIMATION STYLES --- */
-.chat-container {
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  width: 90%;
-  max-width: 300px;
-  margin-bottom: 40px; /* Space between chat and "Can you believe it?" */
-}
-
-.chat-bubble {
-  padding: 10px 15px;
-  border-radius: 20px;
-  max-width: 70%;
-  font-size: 1.2rem;
-  text-align: left; /* Ensures text inside the bubble is left-aligned */
-}
-
-.sent {
-  background-color: #007bff;
-  color: white;
-  align-self: flex-end;
-  border-bottom-right-radius: 5px;
-}
-
-.received {
-  background-color: #e9e9eb;
-  color: black;
-  align-self: flex-start;
-  border-bottom-left-radius: 5px;
-}
-
-.typing {
-  display: flex;
-  align-items: center;
-  gap: 5px;
-  padding: 15px; /* Give typing indicator some space */
-}
-
-.typing span {
-  width: 8px;
-  height: 8px;
-  background-color: #b0b0b0;
-  border-radius: 50%;
-  animation: typing-dots 1.5s infinite;
-}
-
-/* Make dots white for the sent (blue) bubble */
-.sent.typing span {
-  background-color: white;
-}
-
-.typing span:nth-child(2) {
-  animation-delay: 0.2s;
-}
-
-.typing span:nth-child(3) {
-  animation-delay: 0.4s;
-}
-
-@keyframes typing-dots {
-  0%, 60%, 100% {
-    transform: translateY(0);
-  }
-  30% {
-    transform: translateY(-5px);
-  }
-}
-
-.landing-content-wrapper {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
-
-/* --- ORIGINAL AND MODIFIED STYLES --- */
-@keyframes pulse {
-  0% { transform: scale(1); }
-  50% { transform: scale(1.1); }
-  100% { transform: scale(1); }
-}
-
-.slide-out-leave-active {
-  transition: transform 0.7s ease-in-out;
-}
-.slide-out-leave-to {
-  transform: translateY(-100%);
-}
-
-.landing-page {
-  background-color: #4a148c; /* A deeper purple */
-  color: white;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  width: 100vw; /* Use vw to ensure full viewport width */
-  position: fixed; /* Use fixed to ensure it covers the viewport */
-  top: 0;
-  left: 0;
-  padding: 0 15px;
-  box-sizing: border-box;
-}
-
-.landing-page h1 {
-  font-size: 3rem;
-  text-align: center;
-  animation: pulse 1.5s infinite alternate;
-}
-
-/* Media query for mobile responsiveness */
-@media (max-width: 600px) {
-  .landing-page h1 {
-    font-size: 2.5rem;
-  }
-}
-
-
-.arrow-btn {
-  background: none;
-  border: none;
-  font-size: 3rem;
-  color: white;
-  cursor: pointer;
-  margin-top: 20px;
-}
-
-.arrow-btn span {
-  font-size: 4rem;
-}
-
+/* --- MAIN CONTENT STYLES --- */
 .main-content {
   padding: 5rem 20px 20px;
   display: flex;
